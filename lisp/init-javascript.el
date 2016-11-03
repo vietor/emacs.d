ac5c87f239d1fce9e936ebd90de73a46dd099669
Vietor Liu <vietor.liu@gmail.com>
2016-11-03 11:08:42 +0800

    Add some script

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -1,14 +1,24 @@
+;; json
 (require-package 'json-mode)
+
 (add-hook 'json-mode-hook
           (lambda ()
             (setq json-reformat:pretty-string? t)
             (setq deep-buffer-indent-function 'json-mode-beautify)))
 
+;; javascript
 (require-package 'js2-mode)
+
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode) 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+
+(when (executable-find "js-beautify")
+  (require-package 'web-beautify)
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (setq deep-buffer-indent-function 'web-beautify-js))))
 
 (provide 'init-javascript)
