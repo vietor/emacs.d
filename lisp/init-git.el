9b8f8b696143a570e7b5a94e2a7548ab05835e35
Vietor Liu <vietor.liu@gmail.com>
2016-11-09 22:54:15 +0800

    Modify some script

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -1,21 +1,22 @@
-(require-package 'magit)
-(require-package 'gitignore-mode)
-(require-package 'gitconfig-mode)
-(require-package 'git-messenger)
-(require-package 'git-timemachine)
+(unless (version< emacs-version "24.4")
+  (require-package 'magit)
+  (require-package 'gitignore-mode)
+  (require-package 'gitconfig-mode)
+  (require-package 'git-messenger)
+  (require-package 'git-timemachine)
 
-(setq-default
-  magit-process-popup-time 10
-  magit-diff-refine-hunk t
-  magit-completing-read-function 'magit-ido-completing-read)
+  (setq-default
+   magit-process-popup-time 10
+   magit-diff-refine-hunk t
+   magit-completing-read-function 'magit-ido-completing-read)
 
-(add-hook 'git-commit-mode-hook 'goto-address-mode)
-(after-load 'session
-  (when (boundp 'session-mode-disable-list)
-    (add-to-list 'session-mode-disable-list 'git-commit-mode)))
+  (add-hook 'git-commit-mode-hook 'goto-address-mode)
+  (after-load 'session
+    (when (boundp 'session-mode-disable-list)
+      (add-to-list 'session-mode-disable-list 'git-commit-mode)))
 
-(global-set-key (kbd "C-<f12>") 'magit-status)
-(global-set-key (kbd "C-x v f") 'vc-git-grep)
-(global-set-key (kbd "C-x v p") #'git-messenger:popup-message)
+  (global-set-key (kbd "C-<f12>") 'magit-status)
+  (global-set-key (kbd "C-x v f") 'vc-git-grep)
+  (global-set-key (kbd "C-x v p") #'git-messenger:popup-message))
 
 (provide 'init-git)
