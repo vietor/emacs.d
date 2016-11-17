5e7db3a43500cda09bddfa765189dcba83e4e47b
Vietor Liu <vietor.liu@gmail.com>
2016-11-18 00:02:17 +0800

    Modify some script

--- lisp/init-editor.el
+++ lisp/init-editor.el
@@ -110,5 +110,7 @@
 
 (global-set-key (kbd "C-.")     'set-mark-command)
 (global-set-key (kbd "C-x C-.") 'pop-global-mark)
+(global-set-key (kbd "M-g q")   'keyboard-escape-quit)
+
 
 (provide 'init-editor)
