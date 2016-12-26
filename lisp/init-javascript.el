37d198fde11c798279d063a58f4411afd7443f4c
Vietor Liu <vietor.liu@gmail.com>
2016-12-26 13:52:17 +0800

    Modify jsx to web-mode

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -12,8 +12,7 @@
 (add-hook 'js-mode-hook 'js2-minor-mode)
 (add-hook 'js2-mode-hook #'js2-imenu-extras-mode) 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . js2-jsx-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-jsx-mode))
+(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (when (executable-find "js-beautify")
   (require-package 'web-beautify)
--- lisp/init-web.el
+++ lisp/init-web.el
@@ -1,6 +1,10 @@
+;; html
 (require-package 'web-mode)
 (require 'web-mode)
+
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx?\\'" . web-mode))
+
 (setq web-mode-markup-indent-offset 2)
 (setq web-mode-css-indent-offset 2)
 (setq web-mode-code-indent-offset 2)
@@ -9,6 +13,7 @@
 (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
 (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
 
+;; css
 (require-package 'sass-mode)
 
 (provide 'init-web)
