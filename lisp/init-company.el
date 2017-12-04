73858b35cce65db5e34dd322bf88e525579066a5
Vietor Liu <vietor.liu@gmail.com>
2017-12-04 14:39:09 +0800

    Modify some script

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -3,8 +3,11 @@
 (setq completion-cycle-threshold 5)
 
 (require-package 'company)
-(after-load 'company
-  (diminish 'company-mode))
 (add-hook 'after-init-hook 'global-company-mode)
+
+(after-load 'company
+  (diminish 'company-mode)
+  (define-key company-active-map (kbd "C-n") 'company-select-next)
+  (define-key company-active-map (kbd "C-p") 'company-select-previous))
 
 (provide 'init-company)
