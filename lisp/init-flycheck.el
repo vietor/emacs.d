70fd973235520e2ab32c9517b84412a205a96197
Vietor Liu <vietor.liu@gmail.com>
2018-08-08 11:08:30 +0800

    Modify some code

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -1,5 +1,8 @@
 (when (maybe-require-package 'flycheck)
   (global-flycheck-mode)
-  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list))
+  (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
+  (after-load 'flycheck
+    (setq flycheck-gcc-language-standard "c++11")
+    (setq flycheck-clang-language-standard "c++11")))
 
 (provide 'init-flycheck)
