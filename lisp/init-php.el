6c3852d8ffb701edf367c5a210ebc6b2f6f54b4f
Vietor Liu <vietor.liu@gmail.com>
2018-10-09 09:55:51 +0800

    Modify some code

--- lisp/init-php.el
+++ lisp/init-php.el
@@ -1,11 +1,6 @@
 (when (maybe-require-package 'php-mode)
   (maybe-require-package 'smarty-mode)
   (after-load 'php-mode
-    (define-key php-mode-map (kbd "C-.") nil))
-
-  (when (maybe-require-package 'company-php)
-    (after-load 'company
-      (add-hook 'php-mode-hook
-                (lambda () (sanityinc/local-push-company-backend 'company-ac-php-backend))))))
+    (define-key php-mode-map (kbd "C-.") nil)))
 
 (provide 'init-php)
