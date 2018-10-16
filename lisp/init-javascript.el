6c0591a58c0369a605cf5d8f849f98a49366fd0a
Vietor Liu <vietor.liu@gmail.com>
2018-10-16 11:32:16 +0800

    Modify some code

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -10,14 +10,13 @@
 (require-package 'js2-mode)
 (require-package 'rjsx-mode)
 
+(setq-default js2-bounce-indent-p nil)
 (after-load 'js2-mode
+  (js2-imenu-extras-setup)
   (define-key js2-mode-map (kbd "M-.") nil))
 
-(add-hook 'js-mode-hook 'js2-minor-mode)
-(add-hook 'js2-mode-hook #'js2-imenu-extras-mode)
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 (add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
-(add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 
 (when (executable-find "js-beautify")
   (require-package 'web-beautify))
