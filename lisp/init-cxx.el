e84a384e7699b13bc0bd0046350a946ce7090723
Vietor Liu <vietor.liu@gmail.com>
2018-10-18 16:18:39 +0800

    Modify script

--- lisp/init-cxx.el
+++ lisp/init-cxx.el
@@ -1,23 +1,14 @@
 (require 'kernel-c-style)
 (require 'google-c-style)
 
-(c-add-style "kernel" kernel-c-style)
-(c-add-style "google" google-c-style)
-
-(defadvice c-set-style (after tab-width activate)
-  (when indent-tabs-mode
-    (setq tab-width c-basic-offset)))
-
-(defvar cxx-style "kernel")
-
-(defun cxx-mode-setup ()
-  (c-set-style cxx-style))
-
-(add-hook 'c-mode-hook 'cxx-mode-setup)
-(add-hook 'c++-mode-hook 'cxx-mode-setup)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (kernel-set-c-style)
+            (when indent-tabs-mode
+              (setq tab-width c-basic-offset))))
 
 (after-load 'flycheck
-  (add-hook 'c++-mode-hook
+  (add-hook 'c-mode-common-hook
             (lambda ()
               (setq flycheck-gcc-language-standard "c++11")
               (setq flycheck-clang-language-standard "c++11"))))
--- site-lisp/extra-c-style/kernel-c-style.el
+++ site-lisp/extra-c-style/kernel-c-style.el
@@ -11,8 +11,18 @@
   '("linux"
     (indent-tabs-mode . t)
     (c-offsets-alist
-      (arglist-cont-nonempty
+     (arglist-cont-nonempty
       c-lineup-gcc-asm-reg
-      c-lineup-arglist-tabs-only))))
+      c-lineup-arglist-tabs-only)))
+  "Linux Kernel C/C++ Programming Style.")
+
+;;;###autoload
+(defun kernel-set-c-style ()
+  "Set the current buffer's c-style to Linux Kernel C/C++ Programming
+  Style. Meant to be added to `c-mode-common-hook'."
+  (interactive)
+  (setq indent-tabs-mode t)
+  (setq show-trailing-whitespace t)
+  (c-add-style "Kernel" kernel-c-style t))
 
 (provide 'kernel-c-style)
