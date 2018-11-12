3de262a84464fb63b546701dbbc9992999bda83e
Vietor Liu <vietor.liu@gmail.com>
2018-11-12 11:37:13 +0800

    Fix scripts

--- lisp/init-cxx.el
+++ lisp/init-cxx.el
@@ -1,16 +1,22 @@
 (require 'kernel-c-style)
 (require 'google-c-style)
 
+(defun is-cxx-mode ()
+  (or (eq major-mode 'c-mode)
+      (eq major-mode 'c++-mode)))
+
 (add-hook 'c-mode-hook
           (lambda ()
-            (kernel-set-c-style)
+            (when (is-cxx-mode)
+              (kernel-set-c-style))
             (when indent-tabs-mode
               (setq tab-width c-basic-offset))))
 
 (after-load 'flycheck
   (add-hook 'c-mode-hook
             (lambda ()
-              (setq flycheck-gcc-language-standard "c++11")
-              (setq flycheck-clang-language-standard "c++11"))))
+              (when (is-cxx-mode)
+                (setq flycheck-gcc-language-standard "c++11")
+                (setq flycheck-clang-language-standard "c++11")))))
 
 (provide 'init-cxx)
