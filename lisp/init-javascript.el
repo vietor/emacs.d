6d1a8859e91e61a6eed29477fd0b2a8e0ddffc27
Vietor Liu <vietor.liu@gmail.com>
2019-02-26 18:29:36 +0800

    Modify some script

--- lisp/init-javascript.el
+++ lisp/init-javascript.el
@@ -9,28 +9,32 @@
 ;; javascript
 (require-package 'js2-mode)
 (require-package 'rjsx-mode)
-
-(setq-default js2-bounce-indent-p nil)
-(after-load 'js2-mode
-  (js2-imenu-extras-setup)
-  (define-key js2-mode-map (kbd "M-.") nil))
-
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
-
 (when (executable-find "js-beautify")
   (require-package 'web-beautify))
 
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (setq mode-name "JS2")
-            (when (fboundp 'web-beautify-js)
-              (setq deep-buffer-indent-function 'web-beautify-js))))
+(after-load 'js2-mode
+  (js2-imenu-extras-setup)
+  (define-key js2-mode-map (kbd "M-.") nil)
+  (setq-default js2-bounce-indent-p nil)
+  (setq-default js2-mode-show-parse-errors nil
+                js2-mode-show-strict-warnings nil)
+  (add-hook 'js2-mode-hook
+            (lambda ()
+              (setq mode-name "JS2")
+              (when (fboundp 'web-beautify-js)
+                (setq deep-buffer-indent-function 'web-beautify-js))
+              (unless (flycheck-get-checker-for-buffer)
+                (setq-local js2-mode-show-parse-errors t)
+                (setq-local js2-mode-show-strict-warnings t)))))
 
-(add-hook 'js2-jsx-mode-hook
-          (lambda ()
-            (setq mode-name "JS2-JSX")
-            (setq deep-buffer-indent-function nil)))
+(after-load 'rjsx-mode
+  (add-hook 'js2-jsx-mode-hook
+            (lambda ()
+              (setq mode-name "JS2-JSX")
+              (setq deep-buffer-indent-function nil))))
+
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . rjsx-mode))
 
 (after-load 'company-gtags
   (add-to-list 'company-gtags-modes 'js2-mode))
