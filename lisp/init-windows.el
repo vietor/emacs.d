a09bbaa3f2848c547568cd407bd4f1b5f71bc0ee
Vietor Liu <vietor.liu@gmail.com>
2019-06-04 13:05:39 +0800

    Clean some code

--- lisp/init-windows.el
+++ lisp/init-windows.el
@@ -68,33 +68,6 @@
       (set-window-buffer (next-window) other-buffer))))
 (global-set-key (kbd "C-x 4 2") 'split-window-vertically-instead)
 
-(defun smart-last-usage-buffer ()
-  (let ((target-buffer nil) (blist (cdr (buffer-list (selected-frame)))))
-    (catch 'break
-      (while blist
-        (let (name mode (buffer (car blist)))
-          (setq blist (cdr blist))
-          (setq name (buffer-name buffer))
-          (setq mode (buffer-major-mode buffer))
-          (unless (or (string-prefix-p " " name)
-                      (and (string-prefix-p "*" name)
-                           (string-suffix-p "*" name)
-                           (not (member name '("*scratch*" "*shell*"))))
-                      (member mode '(dired-mode gtags-select-mode)))
-            (setq target-buffer buffer)
-            (throw 'break nil)))))
-    target-buffer))
-
-(defun split-window-most-recent-buffer()
-  (interactive)
-  (if (eq last-command 'split-window-most-recent-buffer)
-      (progn
-        (jump-to-register :split-window-most-recent-buffer)
-        (setq this-command 'split-window-most-recent-buffer-unset))
-    (window-configuration-to-register :split-window-most-recent-buffer)
-    (switch-to-buffer-other-window (smart-last-usage-buffer))))
-(global-set-key (kbd "C-<f6>")  'split-window-most-recent-buffer)
-
 (defun switch-to-scratch-buffer ()
   (interactive)
   (switch-to-buffer (get-buffer-create "*scratch*")))
@@ -107,11 +80,6 @@
   (erase-buffer)
   (buffer-enable-undo))
 (global-set-key (kbd "C-<f8>")  'switch-to-empty-scratch-buffer)
-
-(defun switch-to-last-usage-buffer()
-  (interactive)
-  (switch-to-buffer (smart-last-usage-buffer)))
-(global-set-key (kbd "<f6>")    'switch-to-last-usage-buffer)
 
 (defun switch-to-shell-buffer ()
   (interactive)
