f2702fcfccb059ecf6e938e699946291c35d7a86
Vietor Liu <vietor.liu@gmail.com>
2019-06-13 17:10:26 +0800

    Modify some script

--- lisp/init-builtin.el
+++ lisp/init-builtin.el
@@ -41,14 +41,16 @@
 (setq visible-bell nil)
 (setq ring-bell-function 'ignore)
 
+(when (fboundp 'tooltip-mode)
+  (tooltip-mode -1))
 (when (fboundp 'menu-bar-mode)
   (menu-bar-mode -1))
 (when (fboundp 'tool-bar-mode)
   (tool-bar-mode -1))
 (when (fboundp 'scroll-bar-mode)
   (scroll-bar-mode -1))
-(when (fboundp 'tooltip-mode)
-  (tooltip-mode -1))
+(when (fboundp 'horizontal-scroll-bar-mode)
+  (horizontal-scroll-bar-mode -1))
 
 ;; initial gui
 
