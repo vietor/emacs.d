95ce34a8028490fa4215c2f6bf6cfa7922d048ab
Vietor Liu <vietor.liu@gmail.com>
2019-06-13 16:57:10 +0800

    Modify some script

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -7,7 +7,18 @@
 
 (when (maybe-require-package 'magit)
   (setq-default magit-diff-refine-hunk t)
-  (global-set-key (kbd "C-x g") 'magit-status))
+  (global-set-key (kbd "C-x g") 'magit-status)
+
+  (defun magit-vc-print-log (&optional prompt)
+    (interactive "P")
+    (if (and (buffer-file-name)
+             (eq 'Git (vc-backend (buffer-file-name))))
+        (if prompt
+            (magit-log-buffer-file-popup)
+          (magit-log-buffer-file t))
+      (vc-print-log)))
+  (after-load 'vc
+    (define-key vc-prefix-map (kbd "l") 'magit-vc-print-log)))
 
 (after-load 'magit
   (fullframe magit-status magit-mode-quit-window))
