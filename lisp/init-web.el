2bcdce2d2a3b6b0aace34844f3f5db4f9e3c889c
Vietor Liu <vietor.liu@gmail.com>
2019-07-04 17:16:25 +0800

    Modify some code

--- lisp/init-web.el
+++ lisp/init-web.el
@@ -4,22 +4,17 @@
 
 ;; html
 
-(use-package 'web-mode t)
-
+(use-package 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.njk?\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
-
-(add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
-(add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
-(add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
-(add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil))
-
-(defvar web-mode-standard-indent 4)
-(add-hook 'web-mode-hook
-          (lambda ()
-            (setq web-mode-css-indent-offset web-mode-standard-indent)
-            (setq web-mode-code-indent-offset web-mode-standard-indent)
-            (setq web-mode-markup-indent-offset web-mode-standard-indent)))
+(after-load 'web-mode
+  (setq-default  web-mode-css-indent-offset 4
+                 web-mode-code-indent-offset 4
+                 web-mode-markup-indent-offset 4)
+  (add-to-list 'web-mode-indentation-params '("lineup-args" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-calls" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-concats" . nil))
+  (add-to-list 'web-mode-indentation-params '("lineup-ternary" . nil)))
 
 (after-load 'company-dabbrev-code
   (add-to-list 'company-dabbrev-code-modes 'web-mode))
