9a7444569a1f393b0aabc63cd606c1d5fde92bdc
Vietor Liu <vietor.liu@gmail.com>
2019-07-09 10:48:59 +0800

    Modify some scripts

--- lisp/init-textmodes.el
+++ lisp/init-textmodes.el
@@ -2,6 +2,12 @@
 ;;; Commentary:
 ;;; Code:
 
+;; markdown
+
+(use-package 'markdown-mode)
+(add-to-list 'buffer-indent-disabled-modes 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+
 (provide 'init-textmodes)
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
