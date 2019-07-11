0a5c796cdcec0b18f2fcc09e06114df7784132bc
Vietor Liu <vietor.liu@gmail.com>
2019-07-11 15:56:12 +0800

    Modify some codes

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -8,14 +8,14 @@
 (use-package 'company)
 (add-hook 'after-init-hook 'global-company-mode)
 (after-load 'company
-  (dolist (backend '(company-eclim company-semantic))
-    (delq backend company-backends))
+  (setq company-dabbrev-downcase nil
+        company-backends '((company-dabbrev-code company-gtags company-keywords)
+                           company-files company-dabbrev))
   (diminish 'company-mode)
   (define-key company-mode-map (kbd "M-/") 'company-complete)
   (define-key company-active-map (kbd "M-/") 'company-other-backend)
   (define-key company-active-map (kbd "C-n") 'company-select-next)
   (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (setq company-dabbrev-downcase nil)
   (setq-default company-dabbrev-other-buffers 'all
                 company-tooltip-align-annotations t)
   (global-set-key (kbd "M-C-/") 'company-complete))
