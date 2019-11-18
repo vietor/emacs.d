34bedce7e82acaeb779b961fd56a4010e3434324
Vietor Liu <vietor.liu@gmail.com>
2019-11-18 09:40:21 +0800

    Add more codes

---
+++ early-init.el
@@ -0,0 +1,12 @@
+;;; early-init.el --- -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Code:
+
+(setq package-enable-at-startup nil)
+
+(provide 'early-init)
+;; Local Variables:
+;; coding: utf-8
+;; no-byte-compile: t
+;; End:
+;;; early-init.el ends here
