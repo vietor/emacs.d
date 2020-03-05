934ecd83d1b937e64ceb93a9729f55a27b0a47d0
Vietor Liu <vietor.liu@gmail.com>
2020-03-05 17:23:07 +0800

    MOdify some script

--- lisp/init-textmodes.el
+++ lisp/init-textmodes.el
@@ -7,6 +7,11 @@
 (require-package 'markdown-mode)
 (add-to-list 'buffer-indent-disabled-modes 'markdown-mode)
 
+;; yaml
+
+(require-package 'yaml-mode)
+(add-to-list 'auto-mode-alist '("\\.yml\\.erb\\'" . yaml-mode))
+
 (provide 'init-textmodes)
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
