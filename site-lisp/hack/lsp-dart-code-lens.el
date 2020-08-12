098d29180b64cd2337a434d08c16b0cfe1db3853
Vietor Liu <vietor.liu@gmail.com>
2020-08-12 09:53:33 +0800

    Revert "Remove lsp-dart hack"
    This reverts commit f2bb46286a0d9960d7de653be0fafc04efa54967.

---
+++ site-lisp/hack/lsp-dart-code-lens.el
@@ -0,0 +1,10 @@
+;;; lsp-dart-code-lens.el --- -*- lexical-binding: t; -*-
+;;; Commentary:
+;;;   This file was disable the lsp-dart's dap-mode support
+;;; Code:
+
+(defvar lsp-dart-main-code-lens nil)
+(defvar lsp-dart-test-code-lens nil)
+
+(provide 'lsp-dart-code-lens)
+;;; lsp-dart-code-lens.el ends here
