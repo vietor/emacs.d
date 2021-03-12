27d6182dcfa199f119aa1422a18db9b89387554d
Vietor Liu <vietor.liu@gmail.com>
2021-03-12 21:35:01 +0800

    Fix lsp-dart

--- lisp/init-dart.el
+++ lisp/init-dart.el
@@ -9,14 +9,14 @@
   :after dart-mode
   :hook (dart-mode . lsp-deferred)
   :config
-  (setq lsp-dart-outline nil
+  (setq lsp-dart-line-length 160
+        lsp-dart-outline nil
         lsp-dart-closing-labels nil
         lsp-dart-main-code-lens nil
         lsp-dart-test-code-lens nil
         lsp-dart-flutter-outline nil
         lsp-dart-flutter-fringe-colors nil
-        lsp-dart-flutter-widget-guides nil
-        lsp-dart-flutter-executable "flutter --no-version-check"))
+        lsp-dart-flutter-widget-guides nil))
 
 (provide 'init-dart)
 ;; Local Variables:
