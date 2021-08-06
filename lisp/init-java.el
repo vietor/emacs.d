9ed6f4e907d390e112371b8592e7afee603ea3f6
Vietor Liu <vietor.liu@gmail.com>
2021-08-06 16:08:38 +0800

    Remove lsp-mode usage

--- lisp/init-java.el
+++ lisp/init-java.el
@@ -8,19 +8,9 @@
   :when (executable-find "gradle")
   :mode "\\.gradle\\'")
 
-(use-package lsp-java
-  :hook (java-mode . lsp-deferred)
-  :init
-  (setq lsp-java-workspace-dir (expand-file-name "java-workspace" user-space-directory))
-  (after-aproject-change
-   (setq lsp-java-workspace-cache-dir (aproject-store-file "workspace-cache/")))
-  :config
-  (setq lsp-java-autobuild-enabled nil
-        lsp-java-selection-enabled nil
-        lsp-java-save-action-organize-imports nil
-        lsp-java-import-gradle-wrapper-enabled nil
-        lsp-java-format-settings-profile "GoogleStyle"
-        lsp-java-format-settings-url (expand-file-name "etc/eclipse-java-google-style.xml" user-emacs-directory)))
+(use-package java
+  :ensure nil
+  :hook (java-mode . eglot-ensure))
 
 (provide 'init-java)
 ;; Local Variables:
--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -13,65 +13,14 @@
   (add-hook 'eglot-managed-mode-hook
             (lambda ()
               (setq company-backends optimized-company-backends)))
+  (setq-default eglot-workspace-configuration
+                `((:java.format.settings.profile . "GoogleStyle")
+                  (:java.format.settings.url . ,(concat "file:///" (expand-file-name "etc/eclipse-java-google-style.xml" user-emacs-directory)))))
   :init
   (before-aproject-change
    (eglot-shutdown-all))
   (add-to-list 'ya-formatter-beautify-minor-alist
                '(eglot--managed-mode . eglot-format-buffer)))
-
-(use-package lsp-mode
-  :diminish
-  :bind (:map lsp-mode-map
-              ("C-c C-d" . lsp-describe-thing-at-point)
-              ([remap xref-find-definitions] . lsp-find-definition)
-              ([remap xref-find-references] . lsp-find-references))
-  :init
-  (setq lsp-keymap-prefix "C-c l"
-        lsp-server-install-dir user-space-directory)
-  (add-to-list 'ya-formatter-beautify-minor-alist '(lsp-mode . lsp-format-buffer))
-  (before-aproject-change
-   (let ((lsp-restart 'ignore))
-     (mapcar 'delete-process (process-list))))
-  (after-aproject-change
-   (setq lsp-session-file (aproject-store-file "lsp-session")))
-  :config
-  (setq lsp-lens-enable nil
-        lsp-ui-doc-enable nil
-        lsp-ui-sideline-enable nil
-
-        lsp-eldoc-enable-hover nil
-        lsp-completion-show-kind nil
-        lsp-completion-show-detail nil
-
-        lsp-headerline-breadcrumb-enable nil
-        lsp-modeline-code-actions-enable nil
-        lsp-modeline-diagnostics-enable nil
-        lsp-signature-auto-activate nil
-        lsp-signature-render-documentation nil
-
-        lsp-enable-snippet nil
-        lsp-enable-folding nil
-        lsp-enable-indentation nil
-        lsp-enable-on-type-formatting nil
-        lsp-enable-text-document-color nil
-        lsp-enable-symbol-highlighting nil
-
-        lsp-keep-workspace-alive nil
-        lsp-enable-file-watchers nil
-        lsp-enable-dap-auto-configure nil)
-
-  (setq lsp-auto-guess-root t)
-  (defun lsp--suggest-project-root ()
-    aproject-rootdir)
-
-  (defun lsp-install-server (update?))
-  (defun lsp--install-server-internal (client &optional update?)
-    (message "Please install the required language server yourself"))
-  (defun filter-lsp--completing-read (orig-fun &rest args)
-    (if (string-match-p "installed automatically:" (car args))
-        (car (nth 1 args))
-      (apply orig-fun args)))
-  (advice-add 'lsp--completing-read :around #'filter-lsp--completing-read))
 
 (provide 'init-lsp)
 ;; Local Variables:
