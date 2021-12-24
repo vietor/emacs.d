955ab5d4e9260c11a0ca8d95522d3eead6fe4297
Vietor Liu <vietor.liu@gmail.com>
2021-12-24 19:13:33 +0800

    Modify some scripts

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -7,7 +7,17 @@
   :mode ("SConstruct\\'" "SConscript\\'")
   :hook (python-mode . eglot-ensure)
   :init
-  (setq python-shell-interpreter "python3"))
+  (setq python-shell-interpreter "python3")
+
+  (defun python-workspace-configuration()
+    (setq eglot-workspace-configuration
+          '((pylsp
+             (configurationSources . ["flake8"])
+             (plugins
+              (flake8 (enabled . t))
+              (mypy (enabled . t))
+              (pycodestyle (enabled . :json-false)))))))
+  (add-to-list 'eglot-language-configuration-alist '("python" . python-workspace-configuration)))
 
 (use-package pip-requirements)
 
