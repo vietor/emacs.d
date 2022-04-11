37303d5ec05fde8a090331aed5762d93bdaffbd5
Vietor Liu <vietor.liu@gmail.com>
2022-04-11 10:43:54 +0800

    Modify some code

--- lisp/init-dart.el
+++ lisp/init-dart.el
@@ -4,15 +4,7 @@
 
 (use-package dart-mode
   :when (executable-find "dart")
-  :hook (dart-mode . eglot-ensure)
-  :init
-  (defun dart-get-analysis_server()
-    (let* ((dart-exe (executable-find "dart"))
-           (dart-sdk-dir (file-truename (locate-dominating-file dart-exe "bin"))))
-      (concat dart-sdk-dir "bin/cache/dart-sdk/bin/snapshots/analysis_server.dart.snapshot")))
-
-  (add-to-list 'eglot-server-programs
-               `(dart-mode . ("dart" ,(dart-get-analysis_server) "--lsp"))))
+  :hook (dart-mode . eglot-ensure))
 
 (provide 'init-dart)
 ;; Local Variables:
