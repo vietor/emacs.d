883812c62c89134e00911eb29d79a8a553f77f28
Vietor Liu <vietor.liu@gmail.com>
2022-05-07 23:34:24 +0800

    Modify some scripts

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -2,18 +2,14 @@
 ;;; Commentary:
 ;;; Code:
 
-(use-package vertico
-  :hook (after-init . vertico-mode))
+(use-package selectrum
+  :hook (after-init . selectrum-mode))
 
 (use-package orderless
-  :after vertico
   :init
   (defun fix-orderless-in-minibuffer ()
     (setq-local completion-styles '(substring orderless)))
   (add-hook 'minibuffer-setup-hook 'fix-orderless-in-minibuffer))
-
-(use-package marginalia
-  :hook (after-init . marginalia-mode))
 
 (provide 'init-minibuffer)
 ;; Local Variables:
