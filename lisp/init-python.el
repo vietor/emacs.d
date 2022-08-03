93c6216566697aca73fbecb440941ec561063ef1
Vietor Liu <vietor.liu@gmail.com>
2022-08-03 21:45:37 +0800

    Modify some code

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -23,16 +23,6 @@
 
 (use-package pip-requirements)
 
-(use-package py-autopep8
-  :when (executable-find "autopep8")
-  :init
-  (defun python-mode-beautify ()
-    "Deep indent for python."
-    (py-autopep8-buffer)
-    (when system-is-win
-      (ya-formatter-x-clean-eol)))
-  (add-to-list 'ya-formatter-beautify-alist '(python-mode . python-mode-beautify)))
-
 (provide 'init-python)
 ;; Local Variables:
 ;; byte-compile-warnings: (not free-vars unresolved)
